00f5d321853328adb8c07a0db12b91e68d1c11a7
torpeyl <lt321@ic.ac.uk>
2024-01-28 18:25:21 +0000

    Adding the light project page

--- web.js
+++ web.js
@@ -25,13 +25,19 @@ document.getElementById('left-page-button').addEventListener('click', function()
     document.getElementById("resumeDiv").style.display = "block";
 })
 
+
 document.getElementById('project-1').addEventListener('click', function(){
     hideAllDivs();
     document.getElementById('iot-project-div').style.display = "block";
+})
+document.getElementById('project-2').addEventListener('click', function(){
+    hideAllDivs();
+    document.getElementById('stage-light-project-div').style.display = "block";
 })
 
 function hideAllDivs(){
     document.getElementById("resumeDiv").style.display = "none";
     document.getElementById("projectsDiv").style.display = "none";
     document.getElementById("iot-project-div").style.display = "none";
+    document.getElementById("stage-light-project-div").style.display = "none";
 }
