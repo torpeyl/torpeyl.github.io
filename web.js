48272976719a8f482f0a9ba9698081d29f1dc954
torpeyl <93376459+torpeyl@users.noreply.github.com>
2024-02-04 14:12:27 +0000

    Adding algo trading page

--- web.js
+++ web.js
@@ -34,10 +34,15 @@ document.getElementById('project-2').addEventListener('click', function(){
     hideAllDivs();
     document.getElementById('stage-light-project-div').style.display = "block";
 })
+document.getElementById('project-6').addEventListener('click', function(){
+    hideAllDivs();
+    document.getElementById('algo-trading-project-div').style.display = "block";
+})
 
 function hideAllDivs(){
     document.getElementById("resumeDiv").style.display = "none";
     document.getElementById("projectsDiv").style.display = "none";
     document.getElementById("iot-project-div").style.display = "none";
     document.getElementById("stage-light-project-div").style.display = "none";
+    document.getElementById("algo-trading-project-div").style.display = "none";
 }
