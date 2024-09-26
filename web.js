1d7bb2864cbac9628c4b0e510df1eea91487c702
torpeyl <lt321@ic.ac.uk>
2024-09-26 12:24:39 +0100

    Latest Project Added

--- web.js
+++ web.js
@@ -3,6 +3,16 @@ addEventListener("load", (event) => {
     document.getElementById("resumeDiv").style.display = "block";
 })
 
+// Modal Expansion
+document.querySelectorAll('.modal-image').forEach(image => {
+    image.addEventListener('click', function() {
+        const modalImage = document.getElementById('modalImage');
+        modalImage.src = this.src;
+        const modal = new bootstrap.Modal(document.getElementById('imageModal'));
+        modal.show();
+    });
+});
+
 document.getElementById('right-page-button').addEventListener("mouseover", function(){
     this.style.background='#383838';
 })
@@ -27,20 +37,29 @@ document.getElementById('left-page-button').addEventListener('click', function()
 
 
 document.getElementById('project-1').addEventListener('click', function(){
+    window.scrollTo(0, 0);
     hideAllDivs();
     document.getElementById('iot-project-div').style.display = "block";
 })
 document.getElementById('project-2').addEventListener('click', function(){
+    window.scrollTo(0, 0);
     hideAllDivs();
     document.getElementById('stage-light-project-div').style.display = "block";
 })
 document.getElementById('project-6').addEventListener('click', function(){
+    window.scrollTo(0, 0);
     hideAllDivs();
     document.getElementById('algo-trading-project-div').style.display = "block";
 })
 document.getElementById('project-8').addEventListener('click', function(){
+    window.scrollTo(0, 0);
     hideAllDivs();
     document.getElementById('market-indicators-project-div').style.display = "block";
+})
+document.getElementById('project-9').addEventListener('click', function(){
+    window.scrollTo(0, 0);
+    hideAllDivs();
+    document.getElementById('sales-forecasting-project-div').style.display = "block";
 })
 
 function hideAllDivs(){
@@ -50,4 +69,5 @@ function hideAllDivs(){
     document.getElementById("stage-light-project-div").style.display = "none";
     document.getElementById("algo-trading-project-div").style.display = "none";
     document.getElementById("market-indicators-project-div").style.display = "none";
+    document.getElementById("sales-forecasting-project-div").style.display = "none";
 }
